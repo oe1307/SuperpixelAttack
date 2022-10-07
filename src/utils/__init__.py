c95f4348062ec2504518900e2b44de85702e1aa0
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-10-07 17:47:34 +0900

    update

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .config_parser import config_parser
-from .confirmation import confirmation
+from .confirmation import confirmation, ssl_certificate
 from .logging import change_level, setup_logger
 from .processbar import pbar
 from .read_gurobi_log import read_log
@@ -12,6 +12,7 @@ COMMENT = False
 __all__ = [
     "config_parser",
     "confirmation",
+    "ssl_certificate",
     "change_level",
     "setup_logger",
     "pbar",
