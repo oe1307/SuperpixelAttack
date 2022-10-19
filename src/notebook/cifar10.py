fd4a20581dd5b60c7b029c6a4204b25b4d16f04f
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-10-19 13:24:17 +0900

    update

--- src/notebook/cifar10.py
+++ src/notebook/cifar10.py
@@ -1,53 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[43]:
+# In[1]:
 
 
 import json
 
-import numpy as np
-
-# In[44]:
+# In[4]:
 
 
-success_iter = np.load(
-    "/data1/issa/AdEx_BlackBox/result/FGSM/cifar10/Linf/Carmon2019Unlabeled/2022-10-14_15:19:04/success_iter.npy"
-)
-
-
-# In[45]:
-
-
-index0 = np.where(success_iter == 0)  # 元々誤分類
-index1 = np.where(success_iter == 1)  # 1回目で成功
-index2 = np.where(np.logical_and(1 < success_iter, success_iter < 10))  # 2回目以降で成功
-index3 = np.where(success_iter == 11)  # 失敗
-
-
-# In[46]:
-
-
-print(index0[0].shape)
-print(index1[0].shape)
-print(index2[0].shape)
-print(index3[0].shape)
-
-
-# In[47]:
-
-
-index = dict()
-index["clean"] = index0[0].tolist()
-index["easy"] = index1[0].tolist()
-index["hard"] = index2[0].tolist()
-index["fail"] = index3[0].tolist()
-
-
-# In[48]:
-
-
-json.dump(index, open("../../storage/data/cifar10.json", "w"))
+cifar10 = json.load(open("../base/cifar10.json"))
+for k in cifar10:
+    print(k, len(cifar10[k]))
 
 
 # In[ ]:
