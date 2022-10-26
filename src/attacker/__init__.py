0680c46e662e7955112ce1b78a6cbbcc43de81f3
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-10-27 02:52:12 +0900

    update

--- src/attacker/__init__.py
+++ src/attacker/__init__.py
@@ -1,6 +1,6 @@
 from utils import config_parser
 
-from .gen_attack_foolbox import FoolboxGenAttacker
+from .gen_attack import GenAttacker
 
 # proposed method
 
@@ -8,8 +8,8 @@ config = config_parser()
 
 
 def get_attacker():
-    if config.attacker == "FoolboxGenAttacker":
-        attacker = FoolboxGenAttacker()
+    if config.attacker == "GenAttacker":
+        attacker = GenAttacker()
     else:
         raise NotImplementedError(config.attacker)
     return attacker
