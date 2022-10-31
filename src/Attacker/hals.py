a48cffd051448ad6e6ebd9c6a26dd7ae076dd883
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-10-31 19:00:51 +0900

    update

--- src/Attacker/hals.py
+++ src/Attacker/hals.py
@@ -1,6 +1,7 @@
+import torch
 from torch import Tensor
 
-from Base import Attacker
+from Base import Attacker, get_criterion
 from Utils import config_parser, setup_logger
 
 logger = setup_logger(__name__)
@@ -10,7 +11,30 @@ config = config_parser()
 class HALS(Attacker):
     def __init__(self):
         super().__init__()
+        if (config.dataset == "cifar10" and config.initial_split != 4) or (
+            config.dataset == "imagenet" and config.initial_split != 32
+        ):
+            logger.warning(f"{config.dataset}: split = {config.initial_split}")
+        self.criterion = get_criterion()
         self.num_forward = config.steps
 
-    def _attack(self, x: Tensor, y: Tensor) -> Tensor:
-        raise NotImplementedError
+    def _attack(self, x_all: Tensor, y_all: Tensor) -> Tensor:
+        x_adv_all = []
+        for x in x_all:
+            self.forward = 0
+            upper = (x + config.epsilon).clamp(0, 1).clone()
+            lower = (x - config.epsilon).clamp(0, 1).clone()
+            split = config.initial_split
+            batch, c, h, w = x.shape
+            is_upper = torch.zeros(
+                (batch, c, h // split, w // split),
+                dtype=torch.bool,
+                device=config.device,
+            )
+            x_adv = lower.clone()
+            loss = self.criterion(self.model(x_adv), y).clone()
+            self.forward += 1
+            breakpoint()
+            x_adv_all.append(x_adv)
+        x_adv_all = torch.stack(x_adv_all)
+        return x_adv_all
