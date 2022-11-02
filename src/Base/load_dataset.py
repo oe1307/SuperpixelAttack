d162e2b3298f6955021d5feaa9ef2212f306fe9b
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-11-02 13:04:45 +0900

    update

--- src/Base/load_dataset.py
+++ src/Base/load_dataset.py
@@ -1,9 +1,10 @@
 from robustbench.data import load_cifar10, load_cifar100
 
-from Utils import config_parser
+from Utils import config_parser, setup_logger
 
 from ._load_dataset import load_cifar10_easy, load_cifar10_hard, load_imagenet
 
+logger = setup_logger(__name__)
 config = config_parser()
 
 
@@ -12,18 +13,28 @@ def load_dataset(model_name, data_dir):
 
     if config.dataset == "cifar10":
         config.num_classes = 10
+        if config.epsilon == 8 / 255:
+            logger.warning(f"dataset={config.dataset} epsilon={config.epsilon}")
         img, label = load_cifar10(10000, data_dir)
     elif config.dataset == "cifar100":
         config.num_classes = 100
+        if config.epsilon == 8 / 255:
+            logger.warning(f"dataset={config.dataset} epsilon={config.epsilon}")
         img, label = load_cifar100(10000, data_dir)
     elif config.dataset == "imagenet":
         config.num_classes = 1000
+        if config.epsilon == 4 / 255:
+            logger.warning(f"dataset={config.dataset} epsilon={config.epsilon}")
         img, label = load_imagenet(model_name, data_dir)
     elif config.dataset == "cifar10_easy":
         config.num_classes = 10
+        if config.epsilon == 8 / 255:
+            logger.warning(f"dataset={config.dataset} epsilon={config.epsilon}")
         img, label = load_cifar10_easy(model_name, data_dir)
     elif config.dataset == "cifar10_hard":
         config.num_classes = 10
+        if config.epsilon == 8 / 255:
+            logger.warning(f"dataset={config.dataset} epsilon={config.epsilon}")
         img, label = load_cifar10_hard(model_name, data_dir)
     else:
         raise ValueError("Dataset not supported")
