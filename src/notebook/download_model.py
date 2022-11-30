6b4e3b81d0edb8c68ec601ab4c82fa394330c23b
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-11-30 15:52:04 +0900

    download model

---
+++ src/notebook/download_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+
+sys.path.append("../")
+
+
+# In[ ]:
+
+
+from base import get_model
+from utils import config_parser
+
+config = config_parser()
+
+
+# In[ ]:
+
+
+robustbench = [
+    "Wong2020Fast",
+    "Engstrom2019Robustness",
+    "Salman2020Do_R18",
+    "Salman2020Do_R50",
+    "Salman2020Do_50_2",
+]
+config.dataset = "imagenet"
+config.device = "cpu"
+for model in robustbench:
+    get_model("robustbench", model, None, "../../storage/model/")
