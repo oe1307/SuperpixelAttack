ab2177bc2e219c848c1432d0af9a93f00d9554b8
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-12-14 14:24:23 +0900

    update

--- src/notebook/download_model.py
+++ src/notebook/download_model.py
@@ -28,6 +28,7 @@ robustbench = [
     "Salman2020Do_R50",
     "Salman2020Do_50_2",
 ]
+config.norm = "Linf"
 config.device = "cpu"
 for model in robustbench:
     get_model(model, None, "../../storage/model/")
