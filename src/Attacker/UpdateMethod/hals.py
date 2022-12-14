86726390a40b0487782fdfb8c12660faf731fa87
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-12-14 18:47:11 +0900

    update

--- src/Attacker/UpdateMethod/hals.py
+++ src/Attacker/UpdateMethod/hals.py
@@ -2,9 +2,8 @@ import heapq
 
 import numpy as np
 import torch
-from torch import Tensor
 
-from utils import config_parser, pbar
+from utils import config_parser
 
 from .base_method import BaseMethod
 
@@ -14,145 +13,71 @@ config = config_parser()
 class HALS(BaseMethod):
     def __init__(self):
         super().__init__()
+        self.local_search = True
+        if config.update_area == "random_square":
+            raise NotImplementedError("HALS does not support random_square")
 
     def step(self, update_area: np.ndarray, targets):
-        self.is_upper = self.is_upper_best.clone()
-        if config.update_area == "superpixel" and config.channel_wise:
-            pass
-            self.forward += None
-        elif config.update_area == "superpixel":
-            pass
-            self.forward += None
-        elif config.update_area == "random_square" and config.channel_wise:
-            pass
-            self.forward += None
-        elif config.update_area == "random_square":
-            pass
-            self.forward += None
-        elif config.update_area == "split_square" and config.channel_wise:
-            pass
-            self.forward += None
-        elif config.update_area == "split_square":
-            pass
-            self.forward += None
-        pred = self.model(self.x_adv).softmax(dim=1)
-        self.loss = self.criterion(pred, self.y)
-        update = self.loss >= self.best_loss
-        self.is_upper_best[update] = self.is_upper[update]
-        self.x_best[update] = self.x_adv[update]
-        self.best_loss[update] = self.loss[update]
+        if self.local_search:
+            is_upper = self.is_upper_best.clone()
+            loss = self.best_loss.clone()
+            breakpoint()
+            if config.update_area == "superpixel" and config.channel_wise:
+                # insert
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+                # deletion
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+            elif config.update_area == "superpixel":
+                # insert
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+                # deletion
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+            elif config.update_area == "split_square" and config.channel_wise:
+                # insert
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+                # deletion
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+            elif config.update_area == "split_square":
+                # insert
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+                # deletion
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+            x_adv_inverse = torch.where(~is_upper, self.upper, self.lower)
+            pred = self.model(x_adv_inverse).softmax(dim=1)
+            loss_inverse = self.criterion(pred, self.y)
+            update = self.forward < config.step
+            self.forward += update
+            update = np.logical_and(
+                update, (loss_inverse > self.best_loss).cpu().numpy()
+            )
+            self.is_upper_best[update] = ~is_upper[update]
+            self.x_best = x_adv_inverse.clone()
+            self.best_loss[update] = loss_inverse[update]
+            self.local_search = False
+        elif targets.shape[0] == 1:
+            self.local_search = True
         return self.x_best, self.forward
-
-    def insert(self, is_upper_all, all_loss):
-        n_images = is_upper_all.shape[0]
-        max_heap = [[] for _ in range(n_images)]
-        all_elements = (~is_upper_all).nonzero()
-
-        # search in elementary
-        num_batch = np.ceil(all_elements.shape[0] / self.model.batch_size)
-        for i in range(num_batch):
-            pbar.debug(i + 1, num_batch, "insert")
-            start = i * self.model.batch_size
-            end = min((i + 1) * self.model.batch_size, all_elements.shape[0])
-            elements = all_elements[start:end]
-            searched = []
-            is_upper = is_upper_all[elements[:, 0]].clone()
-            for i, (idx, c, h, w) in enumerate(elements):
-                if self.forward[idx] >= config.step:
-                    continue
-                assert is_upper[i, c, h, w].item() is False
-                is_upper[i, c, h, w] = True
-                self.forward[idx] += 1
-                searched.append((i, idx, c, h, w))
-            is_upper = torch.repeat_interleave(is_upper, self.split, dim=2)
-            is_upper = torch.repeat_interleave(is_upper, self.split, dim=3)
-            upper = self.upper[elements[:, 0]]
-            lower = self.lower[elements[:, 0]]
-            x_adv = torch.where(is_upper, upper, lower)
-            pred = self.model(x_adv).softmax(dim=1)
-            loss = self.criterion(pred, self.y[elements[:, 0]])
-            for i, idx, c, h, w in searched:
-                delta = (all_loss[idx] - loss[i]).item()
-                heapq.heappush(max_heap[idx], (delta, (c, h, w)))
-
-        # update
-        for idx, _max_heap in enumerate(max_heap):
-            while len(_max_heap) > 1:
-                delta_hat, element_hat = heapq.heappop(_max_heap)
-                delta_tilde = _max_heap[0][0]
-                if delta_hat <= delta_tilde and delta_hat < 0:
-                    assert is_upper_all[idx][element_hat].item() is False
-                    is_upper_all[idx][element_hat] = True
-                elif delta_hat <= delta_tilde and delta_hat >= 0:
-                    break
-                else:
-                    heapq.heappush(_max_heap, (delta_hat, element_hat))
-        all_loss = self.calculate_loss(is_upper_all)
-        self.forward += 1
-        return is_upper_all, all_loss
-
-    def deletion(self, is_upper_all, all_loss):
-        n_images = is_upper_all.shape[0]
-        max_heap = [[] for _ in range(n_images)]
-        all_elements = is_upper_all.nonzero()
-
-        # search in elementary
-        num_batch = np.ceil(all_elements.shape[0] / self.model.batch_size)
-        for i in range(num_batch):
-            pbar.debug(i + 1, num_batch, "deletion")
-            start = i * self.model.batch_size
-            end = min((i + 1) * self.model.batch_size, all_elements.shape[0])
-            elements = all_elements[start:end]
-            searched = []
-            is_upper = is_upper_all[elements[:, 0]].clone()
-            for i, (idx, c, h, w) in enumerate(elements):
-                if self.forward[idx] >= config.step:
-                    continue
-                assert is_upper[i, c, h, w].item() is True
-                is_upper[i, c, h, w] = False
-                self.forward[idx] += 1
-                searched.append((i, idx, c, h, w))
-            is_upper = torch.repeat_interleave(is_upper, self.split, dim=2)
-            is_upper = torch.repeat_interleave(is_upper, self.split, dim=3)
-            upper = self.upper[elements[:, 0]]
-            lower = self.lower[elements[:, 0]]
-            x_adv = torch.where(is_upper, upper, lower)
-            pred = self.model(x_adv).softmax(dim=1)
-            loss = self.criterion(pred, self.y[elements[:, 0]])
-            for i, idx, c, h, w in searched:
-                delta = (all_loss[idx] - loss[i]).item()
-                heapq.heappush(max_heap[idx], (delta, (c, h, w)))
-
-        # update
-        for idx, _max_heap in enumerate(max_heap):
-            while len(_max_heap) > 1:
-                delta_hat, element_hat = heapq.heappop(_max_heap)
-                delta_tilde = _max_heap[0][0]
-                if delta_hat <= delta_tilde and delta_hat < 0:
-                    assert is_upper_all[idx][element_hat].item() is True
-                    is_upper_all[idx][element_hat] = False
-                elif delta_hat <= delta_tilde and delta_hat >= 0:
-                    break
-                else:
-                    heapq.heappush(_max_heap, (delta_hat, element_hat))
-        loss = self.calculate_loss(is_upper_all)
-        self.forward += 1
-        return is_upper_all, loss
-
-    def calculate_loss(self, is_upper_all: Tensor) -> Tensor:
-        n_images = is_upper_all.shape[0]
-        loss = torch.zeros(n_images, device=config.device)
-        num_batch = np.ceil(n_images / self.model.batch_size)
-        for i in range(num_batch):
-            start = i * self.model.batch_size
-            end = min((i + 1) * self.model.batch_size, n_images)
-            upper = self.upper[start:end]
-            lower = self.lower[start:end]
-            is_upper = is_upper_all[start:end]
-            is_upper = torch.repeat_interleave(is_upper, self.split, dim=2)
-            is_upper = torch.repeat_interleave(is_upper, self.split, dim=3)
-            x_adv = torch.where(is_upper, upper, lower)
-            y = self.y[start:end]
-            pred = self.model(x_adv).softmax(dim=1)
-            loss[start:end] = self.criterion(pred, y)
-        return loss
--- src/Attacker/UpdateMethod/refine_search.py
+++ src/Attacker/UpdateMethod/refine_search.py
@@ -1,11 +1,83 @@
+import heapq
+
 import numpy as np
+import torch
+
+from utils import config_parser
 
 from .base_method import BaseMethod
 
+config = config_parser()
 
-class RefineSearch(BaseMethod):
+
+class HALS(BaseMethod):
     def __init__(self):
         super().__init__()
+        self.local_search = True
+        if config.update_area == "random_square":
+            raise NotImplementedError("HALS does not support random_square")
 
     def step(self, update_area: np.ndarray, targets):
-        raise NotImplementedError()
+        if self.local_search:
+            is_upper = self.is_upper_best.clone()
+            loss = self.best_loss.clone()
+            breakpoint()
+            if config.update_area == "superpixel" and config.channel_wise:
+                # insert
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+                # deletion
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+            elif config.update_area == "superpixel":
+                # insert
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+                # deletion
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+            elif config.update_area == "split_square" and config.channel_wise:
+                # insert
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+                # deletion
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+            elif config.update_area == "split_square":
+                # insert
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+                # deletion
+                update = loss > self.best_loss
+                self.is_upper_best[update] = is_upper[update]
+                self.best_loss[update] = loss[update]
+
+            x_adv_inverse = torch.where(~is_upper, self.upper, self.lower)
+            pred = self.model(x_adv_inverse).softmax(dim=1)
+            loss_inverse = self.criterion(pred, self.y)
+            update = self.forward < config.step
+            self.forward += update
+            update = np.logical_and(
+                update, (loss_inverse > self.best_loss).cpu().numpy()
+            )
+            self.is_upper_best[update] = ~is_upper[update]
+            self.x_best = x_adv_inverse.clone()
+            self.best_loss[update] = loss_inverse[update]
+            self.local_search = False
+        elif targets.shape[0] == 1:
+            self.local_search = True
+        return self.x_best, self.forward
