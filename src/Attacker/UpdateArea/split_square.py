d3cf302f6a1e1569ca4c32d3fe906de7c1c90076
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-12-19 17:30:04 +0900

    fix: error

--- src/Attacker/UpdateArea/split_square.py
+++ src/Attacker/UpdateArea/split_square.py
@@ -12,15 +12,18 @@ class SplitSquare:
 
     def initialize(self, x: Tensor, level: np.ndarray):
         self.batch, self.n_channel, self.height, self.width = x.shape
-        n_split = max(config.initial_split // 2**level, 1)
-        assert self.height % n_split == 0
-        h = self.height // n_split
-        assert self.width % n_split == 0
-        w = self.width // n_split
-        update_area = np.arange(h * w).reshape(1, h, w)
-        update_area = np.repeat(update_area, self.batch, axis=0)
-        update_area = np.repeat(update_area, n_split, axis=0)
-        update_area = np.repeat(update_area, n_split, axis=1)
+        update_area = []
+        for idx in range(self.batch):
+            n_split = max(config.initial_split // 2**level[idx], 1)
+            assert self.height % n_split == 0
+            h = self.height // n_split
+            assert self.width % n_split == 0
+            w = self.width // n_split
+            square = np.arange(1, h * w + 1).reshape(h, w)
+            square = np.repeat(square, n_split, axis=0)
+            square = np.repeat(square, n_split, axis=1)
+            update_area.append(square)
+        update_area = np.stack(update_area)
         assert update_area.shape == (self.batch, self.height, self.width)
         return update_area
 
@@ -30,7 +33,7 @@ class SplitSquare:
         h = self.height // n_split
         assert self.width % n_split == 0
         w = self.width // n_split
-        update_area = np.arange(h * w).reshape(h, w)
+        update_area = np.arange(1, h * w + 1).reshape(h, w)
         update_area = np.repeat(update_area, n_split, axis=0)
         update_area = np.repeat(update_area, n_split, axis=1)
         return update_area
