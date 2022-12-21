b0b6a16f6bfe159c90263f0d5101a071edf6cbfd
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-12-22 06:15:06 +0900

    update

--- src/Attacker/UpdateMethod/refine_search.py
+++ src/Attacker/UpdateMethod/refine_search.py
@@ -12,7 +12,6 @@ class RefineSearch:
     def __init__(self, update_area):
         if config.update_area != "superpixel":
             raise ValueError("Update area is only available for superpixel.")
-        self.max_level = len(config.segments)
         self.update_area = update_area
 
     def set(self, model, criterion):
@@ -25,8 +24,8 @@ class RefineSearch:
         self.upper = upper.clone()
         self.lower = lower.clone()
 
-        self.level = np.zeros(self.batch, dtype=int)
-        self.area = self.update_area.initialize(x, self.level)
+        self.level = 0
+        self.area = self.update_area.initialize(x, np.zeros(self.batch, dtype=int))
         self.targets = []
         for idx in range(self.batch):
             labels = np.unique(self.area[idx])
@@ -35,103 +34,107 @@ class RefineSearch:
             labels = np.repeat(labels, self.n_channel)
             channel_labels = np.stack([channel, labels], axis=1)
             self.targets.append(np.random.permutation(channel_labels))
-        self.x_best = x.clone()
+        self.x_adv = x.clone()
+        self.best_loss = -100 * torch.ones(self.batch, device=config.device)
         self.forward = np.zeros(self.batch, dtype=int)
         return self.forward
 
     def step(self):
-        self.refine(self.area.copy(), self.targets.copy(), self.level.copy())
-        self.level = np.minimum(self.level + 1, self.max_level).astype(int)
+        self.refine(self.area.copy(), self.targets.copy(), self.level)
+        self.level = min(self.level + 1, len(config.segments) - 1)
         for idx in range(self.batch):
-            self.area[idx] = self.update_area.update(idx, self.level[idx])
-            if config.channel_wise:
-                labels = np.unique(self.area[idx])
-                labels = labels[labels != 0]
-                channel = np.tile(np.arange(self.n_channel), len(labels))
-                labels = np.repeat(labels, self.n_channel)
-                channel_labels = np.stack([channel, labels], axis=1)
-                self.targets[idx] = np.random.permutation(channel_labels)
-            else:
-                labels = np.unique(self.area[idx])
-                labels = labels[labels != 0]
-                self.targets[idx] = np.random.permutation(labels)
-        return self.x_best, self.forward
+            self.area[idx] = self.update_area.update(idx, self.level)
+            labels = np.unique(self.area[idx])
+            labels = labels[labels != 0]
+            channel = np.tile(np.arange(self.n_channel), len(labels))
+            labels = np.repeat(labels, self.n_channel)
+            channel_labels = np.stack([channel, labels], axis=1)
+            self.targets[idx] = np.random.permutation(channel_labels)
+        return self.x_adv, self.forward
 
     def refine(self, area, targets, level):
         n_targets = np.array([len(t) for t in targets])
-        loss_storage = [[] for _ in range(self.batch)]
+        upper_loss = [[] for _ in range(self.batch)]
+        lower_loss = [[] for _ in range(self.batch)]
         for t in range(n_targets.max()):
-            is_upper = self.is_upper_best.clone()
-            if config.channel_wise:
-                for idx in range(self.batch):
-                    if n_targets[idx] <= t or self.forward[idx] >= config.step:
-                        continue
-                    c, label = targets[idx][t]
-                    is_upper[idx, c, area[idx] == label] = ~is_upper[
-                        idx, c, area[idx] == label
-                    ]
-                    self.forward[idx] += 1
-            else:
-                for idx in range(self.batch):
-                    if n_targets[idx] <= t or self.forward[idx] >= config.step:
-                        continue
-                    label = targets[idx][t]
-                    is_upper.permute(0, 2, 3, 1)[
-                        idx, area[idx] == label
-                    ] = ~is_upper.permute(0, 2, 3, 1)[idx, area[idx] == label]
-                    self.forward[idx] += 1
-            x_adv = torch.where(is_upper, self.upper, self.lower)
-            pred = self.model(x_adv).softmax(dim=1)
-            loss = self.criterion(pred, self.y)
+            x_upper = self.x_adv.clone()
+            x_lower = self.x_adv.clone()
             for idx in range(self.batch):
-                if n_targets[idx] > t:
-                    loss_storage[idx].append(loss[idx].item())
+                if n_targets[idx] <= t or self.forward[idx] >= config.step:
+                    continue
+                c, label = targets[idx][t]
+                x_upper[idx, c, area[idx] == label] = self.upper[
+                    idx, c, area[idx] == label
+                ]
+                x_lower[idx, c, area[idx] == label] = self.lower[
+                    idx, c, area[idx] == label
+                ]
+            pred = self.model(x_upper).softmax(dim=1)
+            _upper_loss = self.criterion(pred, self.y)
+            pred = self.model(x_lower).softmax(dim=1)
+            _lower_loss = self.criterion(pred, self.y)
+            for idx in range(self.batch):
+                if n_targets[idx] <= t or self.forward[idx] >= config.step:
+                    continue
+                upper_loss[idx].append(_upper_loss[idx].item())
+                lower_loss[idx].append(_lower_loss[idx].item())
+                if level == 0:
+                    self.forward[idx] += 2
+                else:
+                    self.forward[idx] += 1
             pbar.debug(
                 t + 1,
-                max(n_targets),
-                f"level = {level.max()}",
-                f"forward = {self.forward.mean():.2f}",
+                n_targets.max(),
+                f"{level = }",
+                f"{self.forward.mean() = :.2f}",
             )
             if np.logical_or(n_targets <= t, self.forward >= config.step).all():
                 logger.debug("")
                 break
-        loss_storage = [np.array(loss) for loss in loss_storage]
-        indicies = [loss.argsort()[::-1] for loss in loss_storage]
+
+        lower_loss = [torch.tensor(loss) for loss in lower_loss]
+        upper_loss = [torch.tensor(loss) for loss in upper_loss]
+        loss_storage = [
+            torch.stack([lower_loss[idx], upper_loss[idx]]) for idx in range(self.batch)
+        ]
+        u_is_better = [loss.argmax(dim=0).to(bool) for loss in loss_storage]
+        loss_storage = [loss.max(dim=0)[0] for loss in loss_storage]
+        indices = [loss.argsort(descending=True) for loss in loss_storage]
+        n_indices = np.array([len(idx) for idx in indices])
         level += 1
-        for t in range(n_targets.max()):
+        for t in range(n_indices.max()):
             next_targets = targets.copy()
             next_area = area.copy()
             for idx in range(self.batch):
-                if len(indicies[idx]) > t:
-                    loss = loss_storage[idx][indicies[idx][t]]
+                if len(indices[idx]) > t:
+                    loss = loss_storage[idx][indices[idx][t]]
                     update = (loss >= self.best_loss[idx]).item()
-                    if config.channel_wise and update:
-                        c, label = targets[idx][indicies[idx][t]]
-                        self.is_upper_best[
+                    upper_update = u_is_better[idx][indices[idx][t]].item() & update
+                    lower_update = ~u_is_better[idx][indices[idx][t]].item() & update
+                    c, label = targets[idx][indices[idx][t]]
+                    if upper_update:
+                        self.x_adv[idx, c, area[idx] == label] = self.upper[
                             idx, c, area[idx] == label
-                        ] = ~self.is_upper_best[idx, c, area[idx] == label]
-                    elif update:
-                        label = targets[idx][indicies[idx][t]]
-                        self.x_best.permute(0, 2, 3, 1)[
-                            idx, area[idx] == label
-                        ] = self.lower.permute(0, 2, 3, 1)[idx, area[idx] == label]
-                    if self.forward[idx] < config.step and level.max() < self.max_level:
+                        ]
+                    elif lower_update:
+                        self.x_adv[idx, c, area[idx] == label] = self.lower[
+                            idx, c, area[idx] == label
+                        ]
+                    if (
+                        self.forward[idx] < config.step
+                        and level < len(config.segments) - 1
+                    ):
                         next_area[idx] = self.update_area.update(idx, level[idx])
                         pair = np.stack(
                             [area[idx].reshape(-1), next_area[idx].reshape(-1)]
                         ).T
                         pair = np.unique(pair, axis=0)
-                        if config.channel_wise:
-                            c, label = targets[idx][indicies[idx][t]]
-                            labels = pair[pair[:, 0] == label][:, 1]
-                            channel = np.ones_like(labels) * c
-                            _target = np.stack([channel, labels], axis=1)
-                            next_targets[idx] = np.random.permutation(_target)
-                        else:
-                            label = targets[idx][indicies[idx][t]]
-                            labels = pair[pair[:, 0] == label][:, 1]
-                            next_targets[idx] = np.random.permutation(labels)
+                        c, label = targets[idx][indices[idx][t]]
+                        labels = pair[pair[:, 0] == label][:, 1]
+                        channel = np.ones_like(labels) * c
+                        _target = np.stack([channel, labels], axis=1)
+                        next_targets[idx] = np.random.permutation(_target)
                 else:
                     next_targets[idx] = np.array([])
-            if self.forward.min() < config.step and level.max() < self.max_level:
+            if self.forward.min() < config.step and level < len(config.segments) - 1:
                 self.refine(next_area.copy(), next_targets.copy(), level.copy())
